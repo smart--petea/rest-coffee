89c57b01e710432eed1fc658fff91acd22ac3bce
Petru <smart__petea@mail.ru>
2020-06-25 14:30:28 +0300

    postgres

--- cmd/main.go
+++ cmd/main.go
@@ -4,19 +4,38 @@ import (
     "net/http"
     "github.com/labstack/echo/v4"
     "github.com/labstack/echo/v4/middleware"
+
+    "github.com/go-pg/pg/v10"
+    "github.com/go-pg/pg/v10/orm"
 )
 
 func main() {
+    db := pg.Connect(&pg.Options{
+        Addr: ":5999",
+        User:  "postgres",
+        Password: "postgres"
+    })
+    defer db.Close()
+
+
+
+
     e := echo.New()
 
     e.Use(middleware.Logger())
     e.Use(middleware.Recover())
 
-    e.GET("/", hello)
+    e.POST("/order", createOrder)
 
     e.Logger.Fatal(e.Start(":1323"))
 }
 
-func hello(c echo.Context) error {
-    return c.String(http.StatusOK, "Hello, World!")
+type Order struct {
+}
+
+type OrderItem struct {
+}
+
+func createOrder(c echo.Context) error {
+    return c.String(http.StatusOK, "Order created")
 }
