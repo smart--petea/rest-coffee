e5bfba5c8d5ae5fe74a1451c0fae9093f4c45c32
Petru <smart__petea@mail.ru>
2020-06-27 22:57:06 +0300

    OrderItem

--- cmd/main.go
+++ cmd/main.go
@@ -35,24 +35,26 @@ func main() {
     e.Logger.Fatal(e.Start(":9991"))
 }
 
-type OrderItem struct {
+type CreateOrderBody struct {
+    Items []*OrderItem `json:"items"`
 }
 
 func createOrder(c echo.Context) error {
     db := getDb()
     defer db.Close()
 
-    order := new(Order)
-    if err := c.Bind(order); err != nil {
-        return nil
-    }
+    log.Printf("\n45\n")
 
-    if order.ID != 0 {
+    createOrderBody := new(CreateOrderBody)
+    if err := c.Bind(createOrderBody); err != nil {
+        log.Printf("%v\n", err)
         httpError := echo.ErrBadRequest 
-        httpError.Message = "ID should not be set"
+        httpError.Message = fmt.Sprintf("%s", err)
         return httpError
     }
 
+    log.Printf("\n50\n")
+    order := new(Order)
     err := db.Insert(order)
     if err != nil {
         log.Printf("%v\n", err)
@@ -60,6 +62,28 @@ func createOrder(c echo.Context) error {
         httpError.Message = fmt.Sprintf("%s", err)
         return httpError
     }
+
+    order.Items = make([]*OrderItem, 0, len(createOrderBody.Items))
+    log.Printf("\n%d\n", len(createOrderBody.Items))
+
+    log.Printf("\n60\n")
+    for _, orderItem := range createOrderBody.Items {
+        orderItem.OrderId = order.ID
+        err := db.Insert(orderItem)
+        if err != nil {
+            log.Printf("%v\n", err)
+            httpError := echo.ErrBadRequest 
+            httpError.Message = fmt.Sprintf("%s", err)
+            return httpError
+        }
+
+        order.Items = append(order.Items, orderItem)
+    }
+
+    log.Printf("\n71\n")
+
+    //todo transaction
+    //todo array of order items
 
     orderJson, err := json.Marshal(order)
     if err != nil {
@@ -71,8 +95,9 @@ func createOrder(c echo.Context) error {
 }
 
 type Order struct {
-    ID int `pg:"id,pk" json:"id"`
-    CreatedAt time.Time `pg:"created_at" pg:"default:now()" json:"created_at"`
+    ID int `json:"id" pg:"id,pk"`
+    Items []*OrderItem `json:"items"`
+    CreatedAt time.Time `json:"created_at" pg:"created_at" pg:"default:now()"`
 }
 
 type Good struct {
@@ -80,6 +105,15 @@ type Good struct {
     CreatedAt time.Time `json:"created_at" pg:"created_at" pg:"default:now()"`
     IsDeleted bool `json:"is_deleted" pg:"is_deleted", pg:"default:false"`
     Name string `json:"name" pg:"name"`
+}
+
+type OrderItem struct {
+    ID int `json:"id" pg:"id,pk"`
+    Quantity float32 `json:"quantity", pg:"quantity"`
+    CreatedAt time.Time `json:"created_at" pg:"created_at" pg:"default:now()"`
+    IsDeleted bool `json:"is_deleted" pg:"is_deleted", pg:"default:false"`
+    GoodId int `json:"good_id" pg:"good_id,fk:Good"`
+    OrderId int `json:"order_id" pg:"order_id,fk:Order"`
 }
 
 func createGood(c echo.Context) error {
